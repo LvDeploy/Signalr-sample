d04c6496beea9f4937a394631bb9f3a57a6dcce1
Levi Oliveira <underlevi@hotmail.com>
2019-06-10 02:15:46 -0300

    messagepack and more hub events added

--- SignalRCore/SignalRCore/Controllers/MessageController.cs
+++ SignalRCore/SignalRCore/Controllers/MessageController.cs
@@ -3,40 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRCore.Hubs;
+using SignalRCore.Models;
 
 namespace SignalRCore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class MessageController : ControllerBase
     {
-        // GET api/values
+        private IHubContext<ApplicationHub> _appHub;
+
+        public MessageController(IHubContext<ApplicationHub> appHub)
+        {
+            _appHub = appHub;
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("")]
+        public Task Post(ChatMessage value)
         {
+            return _appHub.Clients.All.SendAsync("Send", value.Message);
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- SignalRCore/SignalRCore/Hubs/ApplicationHub.cs
+++ SignalRCore/SignalRCore/Hubs/ApplicationHub.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace SignalRCore.Hubs
 {
     public class ApplicationHub : Hub
     {
+        public Task Send(ChatMessage chat)
+        {
+            return Clients.All.SendAsync("Send", chat.Message);
+        }
+
+        public ChannelReader<int> CountDown(int from)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteToChannel(channel.Writer, from); //Descartes
+
+            return channel.Reader;
+
+            //função local
+            async Task WriteToChannel(ChannelWriter<int> writer, int thing)
+            {
+                for (int i = thing; i >= 0; i--)
+                {
+                    await writer.WriteAsync(i);
+                    await Task.Delay(1000);
+                }
+
+                writer.Complete();
+            }
+        }
     }
 }
